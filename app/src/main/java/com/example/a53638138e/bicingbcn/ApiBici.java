58c1090297746339450566c5c4b3eb924cae9c54
Meirinf <meirin.f@gmail.com>
2017-01-31 17:31:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a53638138e/bicingbcn/ApiBici.java
@@ -0,0 +1,24 @@
+package com.example.a53638138e.bicingbcn;
+
+import android.net.Uri;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by 53638138e on 31/01/17.
+ */
+
+public class ApiBici {
+
+
+    private static String url = "http://wservice.viabicing.cat/v2/stations";
+    private static ArrayList<EstacionesBici> estacionesBicis = new ArrayList<>();
+
+
+
+
+}
---
+++ app/src/main/java/com/example/a53638138e/bicingbcn/EstacionesBici.java
@@ -0,0 +1,130 @@
+package com.example.a53638138e.bicingbcn;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by 53638138e on 31/01/17.
+ */
+
+public class EstacionesBici  implements Serializable {
+
+    //Variables
+
+    int idEstacion; //id
+    double lat;  //Latitud
+    double lon; //Longitud
+    String streetName; //Nombre de calle
+    int streetNumber; //Numero
+    int altitude; //Altitud
+    int slots;   //espacios disponibles
+    int bikes;   //Bicis disponibles
+    ArrayList<Integer> nearbyStations; //Estaciones cercanas
+    boolean status;  //Estado de la estacion cerrada/abierta
+
+
+    //Constructors
+
+    public EstacionesBici(int idEstacion, double lat, double lon, String streetName, int streetNumber, int slots, int altitude, int bikes, ArrayList<Integer> nearbyStations, boolean status) {
+        this.idEstacion = idEstacion;
+        this.lat = lat;
+        this.lon = lon;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.slots = slots;
+        this.altitude = altitude;
+        this.bikes = bikes;
+        this.nearbyStations = nearbyStations;
+        this.status = status;
+    }
+
+    public EstacionesBici() {
+    }
+
+
+    //Getters
+
+    public int getIdEstacion() {
+        return idEstacion;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public int getSlots() {
+        return slots;
+    }
+
+    public int getBikes() {
+        return bikes;
+    }
+
+    public ArrayList<Integer> getNearbyStations() {
+        return nearbyStations;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    //Setters
+
+
+    public void setIdEstacion(int idEstacion) {
+        this.idEstacion = idEstacion;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
+
+    public void setSlots(int slots) {
+        this.slots = slots;
+    }
+
+    public void setBikes(int bikes) {
+        this.bikes = bikes;
+    }
+
+    public void setNearbyStations(ArrayList<Integer> nearbyStations) {
+        this.nearbyStations = nearbyStations;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
